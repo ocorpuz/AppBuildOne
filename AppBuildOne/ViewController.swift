351fb77326c51d6719f02490ef2477d7679f5ef2
ocorpuz <59846122+ocorpuz@users.noreply.github.com>
2020-02-13 09:56:44 -1000

    Initial Commit

---
+++ AppBuildOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppBuildOne
+//
+//  Created by CM Student on 2/13/20.
+//  Copyright © 2020 UHWO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
